4d1646e8ce9ef918755b6891a623d9a4adcc58f5
Michal Bartosik <msbartosik@gmail.com>
2021-08-29 16:52:25 +0200

    create mobile nav and toggling functionality

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,15 @@
-console.log("it works");
+const burgerBtn = document.querySelector(
+  '[data-element="burger-menu-trigger"]'
+);
+const nav = document.querySelector(".navbar");
+
+burgerBtn.addEventListener("click", () => {
+  nav.classList.toggle("menu-active");
+  if (nav.classList.contains("menu-active")) {
+    burgerBtn.setAttribute("aria-label", "Close Menu");
+    burgerBtn.setAttribute("aria-expanded", true);
+  } else {
+    burgerBtn.setAttribute("aria-label", "Open Menu");
+    burgerBtn.setAttribute("aria-expanded", false);
+  }
+});
